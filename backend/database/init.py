b718bbb18fd5bd96ce707c06a42ec9b9d268486c
denissearias975-ai <denissearias975@gmail.com>
2026-09-17 23:32:21 +0000

    agregacion de variables de entorno

--- backend/database/init.py
+++ backend/database/init.py
@@ -1,10 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql+asyncpg://usuario:password@localhost/mi_db"
-# o para SQLite: "sqlite+aiosqlite:///./mi_base.db"
-
-engine = create_async_engine(DATABASE_URL, echo=True)
+from config import settings
+engine = create_async_engine(settings.database, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
